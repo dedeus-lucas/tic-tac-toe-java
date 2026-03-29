64ce4305a46a0435def174b00e37e098186bc732
Lucas Silva de Deus <silvadedeus.lucas@gmail.com>
2026-03-29 19:23:08 -0300

    feat: improve user input to use 1-3 range instead of 0-2

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ public class Main {
         while (true) {
             game.getBoard().printBoard();
 
-            System.out.println("Jogador " + game.getCurrentPlayer() + ", informe linha e coluna (0 a 2):");
+            System.out.println("Jogador " + game.getCurrentPlayer() + ", informe linha e coluna (1 a 3):");
 
-            int row = scanner.nextInt();
-            int col = scanner.nextInt();
+            int row = scanner.nextInt() - 1;
+            int col = scanner.nextInt() - 1;
 
             if (game.getBoard().isPositionEmpty(row, col)) {
                 game.getBoard().makeMove(row, col, game.getCurrentPlayer());
