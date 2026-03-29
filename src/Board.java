4df8f35089d2876eaf7738d7fbd802cb0753c845
Lucas Silva de Deus <silvadedeus.lucas@gmail.com>
2026-03-29 18:18:14 -0300

    feat: implement board structure and rendering

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,25 @@
 public class Board {
+    private char[][] grid;
+
+    public Board() {
+        grid = new char[3][3];
+        initialize();
+    }
+
+    private void initialize() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                grid[i][j] = ' ';
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(
+                    grid[i][0] + " | " + grid[i][1] + " | " + grid[i][2]
+            );
+            if (i < 2) System.out.println("--+---+--");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tic Tac Toe started");
+        Board board = new Board();
+        board.printBoard();
     }
 }
